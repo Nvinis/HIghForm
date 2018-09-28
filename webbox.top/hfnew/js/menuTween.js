c8315e7a4a2e493f7756c04f72acee523e4263d0
Nvinis <nvinis@mail.ru>
2018-09-28 18:03:26 +0300

    @nvinis

--- webbox.top/hfnew/js/menuTween.js
+++ webbox.top/hfnew/js/menuTween.js
@@ -5,22 +5,22 @@ $(document).ready(function ($) {
 
 
 
-    // build menu part 1
-    var menu_tween_1 = new TimelineMax()
-
-        .to('#brokers-nav', 0.1, {position:'initial', bottom: 'auto', top: 'initial'});
-
-
-    var sceneMenu1 = new ScrollMagic.Scene({
-        triggerElement: "#brokers",
-        triggerHook: trigerPositionMenu
-    })
-
-        .setTween(menu_tween_1)
-        .addIndicators({name: "menu1 ", colorStart: "#FFFFFF", colorTrigger:"#245af4"})
-        .addTo(controller);
-
-    // end build menu part 1
+    // // build menu part 1
+    // var menu_tween_1 = new TimelineMax()
+    //
+    //     .to('#brokers-nav', 0.1, {position:'initial', bottom: 'auto', top: 'initial'});
+    //
+    //
+    // var sceneMenu1 = new ScrollMagic.Scene({
+    //     triggerElement: "#brokers",
+    //     triggerHook: trigerPositionMenu
+    // })
+    //
+    //     .setTween(menu_tween_1)
+    //     .addIndicators({name: "menu1 ", colorStart: "#FFFFFF", colorTrigger:"#245af4"})
+    //     .addTo(controller);
+    //
+    // // end build menu part 1
 
 
     var tl1 = new TimelineMax({repeat: -1});
